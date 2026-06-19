65ad8163b018aa65eb9c7bc8027fba964b856618
NEELAMOHAN R <neelamohan6cs@gmail.com>
2026-06-19 15:30:12 +0530

    Create Training.java

---
+++ java/Training.java
@@ -0,0 +1,66 @@
+// // // Online Java Compiler
+// // // Use this editor to write, compile and run your Java code online
+// // import java.util.HashMap;
+// // class Main {
+    
+// //     public static void count(String a){
+// //       HashMap<Character, Integer> counter = new HashMap<>();
+// //       for(int i=0;i<a.length();i++){
+// //           char ch = a.charAt(i);
+// //           if(counter.containsKey(ch)){
+// //               counter.put(ch,counter.get(ch)+1);
+// //           }else{
+// //               counter.put(ch,1);
+// //           }  
+           
+// //       }
+// //       System.out.println(counter);
+// //     }
+    
+    
+// //     public static void main(String[] args) {
+// //         String a="banana";
+// //         System.out.println(a);
+// //         count(a);
+// //     }
+// // }
+
+
+// public class Main{
+    
+//     public static void ans(String s){
+//         for(int i =0;i<s.length();i++){
+//             int count =0;
+//             for (int j = i+1;j<s.length();j++){
+//                 if(s.charAt(i)==s.charAt(j)){
+//                     count ++;
+//                 }
+//             }
+//             if(count>=1){
+//                 System.out.println(s.charAt(i));
+//             }
+//         }
+        
+//     }
+    
+    
+//     public static void main(String[] args){
+//         String s = "programming";
+//         ans(s);
+//         //System.out.println(s);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
